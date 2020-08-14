cd0724d0f443ad6ec99732bafeb079269e1e872d
Ethan Tatum <ethantatum81@gmail.com>
2020-08-14 14:30:00 -0700

    added youtube API base axios call

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBhOzi3BiBfKvFVCwKdvLdnr45ooLlXMjc";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 8,
+    key: KEY,
+  },
+});
