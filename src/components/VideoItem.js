ee6e1a1ae1deb8e958858f6f1490e27d6534e49e
Ethan Tatum <ethantatum81@gmail.com>
2020-09-15 21:08:22 -0700

    added VideoDetail and styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,21 @@
 import "./VideoItem.css";
 import React from "react";
+import DOMPurify from "dompurify";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
+  // DOMPurify takes outside HTML and sanitizes it to prevent XSS attacks
+  const cleanHTML = DOMPurify.sanitize(video.snippet.title);
   return (
-    <div className=" video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+    <div onClick={() => onVideoSelect(video)} className=" video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
       <div className="content">
-        <div className="header">{video.snippet.title}</div>
+        <div className="header">
+          <span dangerouslySetInnerHTML={{ __html: cleanHTML }}></span>
+        </div>
       </div>
     </div>
   );
